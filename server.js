45947563274307f61844270065f4d2a50cb75c5d
Eoin Houlihan <ehoulih@tcd.ie>
2014-12-13 14:10:51 +0000

    Create workers

--- server.js
+++ server.js
@@ -2,16 +2,34 @@ var express = require('express');
 var server = express();
 var childprocess = require('child_process');
 var sqlite3 = require('sqlite3');
+var crypto = require('crypto');
 var db = null;
+server.use(require('body-parser').json());
+
+var workers = [];
 
 childprocess.exec('python makedb.py', function(error, stdout, stderr) {
   db = new sqlite3.Database('names.db');
 });
 
 server.get('/', function(req, res) {
-  res.send('Hi');
+  res.send('Number of connected workers: ' + workers.length);
+});
+
+server.post('/join', function(req, res) {
+  var mips = req.body.mips;
+  var response = {};
+
+  var id = crypto.randomBytes(20).toString('hex');
+  response.id = id;
+  workers.push({
+    id: id,
+    lastHeartbeat: new Date(),
+    blockId: null
+  });
+  res.send(response);
 });
 
 var port = 3000;
 server.listen(port);
-console.log("Server listening on port " + port);
+console.log('Server listening on port ' + port);
